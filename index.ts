b937257c1d9c9fb02b36f1b7d565ff96fc6139d1
yasuaki640 <yasuaki640@gmail.com>
2024-07-20 00:08:59 +0900

    Refactor file handling for input and output directories in convert function and add recursive search for mixed wav files in main function

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import ffmpeg from "fluent-ffmpeg";
 import { parseArgs } from "util";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 const codecExtensions = {
   libmp3lame: "mp3",
@@ -9,14 +11,30 @@ const codecExtensions = {
 type Codec = keyof typeof codecExtensions;
 
 function convert(
-  inputPath: string,
-  codec: Codec = "libmp3lame",
-  bitrate: number = 192,
+  inputFilePath: string,
+  {
+    codec = "libmp3lame",
+    bitrate = 192,
+    outputDir,
+  }: {
+    codec?: Codec;
+    bitrate?: number;
+    outputDir?: string;
+  } = {},
 ) {
-  const outputExt = codecExtensions[codec];
-  const outputName = inputPath.replace(/\.(wav|WAV)$/i, `.${outputExt}`);
+  const outputFileExt = codecExtensions[codec];
+  const fileName = path
+    .basename(inputFilePath)
+    .replace(path.extname(inputFilePath), `.${outputFileExt}`);
+  const finalOutputDir = outputDir || path.dirname(inputFilePath);
 
-  ffmpeg(inputPath)
+  if (!fs.existsSync(finalOutputDir)) {
+    fs.mkdirSync(finalOutputDir, { recursive: true });
+  }
+
+  const outputName = path.join(finalOutputDir, fileName);
+
+  ffmpeg(inputFilePath)
     .audioCodec(codec)
     .audioBitrate(bitrate)
     .save(outputName)
@@ -27,29 +45,63 @@ function convert(
       console.log("An error occurred: " + err.message);
     })
     .on("end", () => {
-      console.log("Processing finished !");
+      console.log("Processing finished: " + outputName);
     });
 }
 
+function searchMixedWavRecursively(inputDir: string): string[] {
+  const targets: string[] = [];
+  const files = fs.readdirSync(inputDir);
+  for (const file of files) {
+    const path = `${inputDir}/${file}`;
+    if (fs.statSync(path).isDirectory()) {
+      targets.push(...searchMixedWavRecursively(path));
+    } else if (file.match(/TrLR\.WAV$/i)) {
+      targets.push(path);
+    }
+  }
+  return targets;
+}
+
 async function main() {
-  const { positionals } = parseArgs({
+  const { values } = parseArgs({
     args: Bun.argv,
+    options: {
+      inputDir: {
+        short: "i",
+        type: "string",
+      },
+      outputDir: {
+        short: "o",
+        type: "string",
+      },
+    },
+    strict: true,
     allowPositionals: true,
   });
 
-  if (positionals.length < 3) {
-    console.error("Please specify input file path");
+  const inputDir = values.inputDir;
+  if (!inputDir) {
+    console.error("Please specify input directory.");
+    process.exit(1);
+  }
+  if (!fs.existsSync(inputDir)) {
+    console.error(`Directory ${inputDir} does not exist.`);
     process.exit(1);
   }
 
-  const inputPath = positionals[2];
-  const exists = await Bun.file(inputPath).exists();
-  if (!exists) {
-    console.error("File not found");
+  const targets = searchMixedWavRecursively(inputDir);
+  if (targets.length === 0) {
+    console.error(`No mixed wav files found in ${inputDir}.`);
     process.exit(1);
   }
 
-  convert(inputPath);
+  const outputDir = values.outputDir || inputDir;
+  for (const target of targets) {
+    convert(target, {
+      outputDir,
+    });
+  }
 }
 
 main();
